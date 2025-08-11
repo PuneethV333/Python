10bead80eab92770ad08d008f85a1a21b3d44e15
PuneethV333 <puneethvijayakumar333@gmail.com>
2025-08-11 19:59:06 +0530

    day-2

---
+++ day-4/1.py
@@ -0,0 +1,5 @@
+print("Hello world") #printing hello world
+# single line comment
+'''
+    multi line comment
+'''
---
+++ day-4/2.py
@@ -0,0 +1,4 @@
+print("hello Punneth\nPreetham") #\n -> new line  escape sequence
+print("hello Punneth\"Preetham\" mamatha ") #\" \" -> doule inverted comma  escape sequence
+print("hello Punneth\t mamatha ") #\t -> tab space
+print("hey",3,4,sep="_",end=" ") #saparates the each elemets by its value
