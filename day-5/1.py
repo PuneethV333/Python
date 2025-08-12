acc52ab692938d91640cbe5a13efcbb853480da9
PuneethV333 <puneethvijayakumar333@gmail.com>
2025-08-12 18:34:54 +0530

    day-6

---
+++ day-5/1.py
@@ -0,0 +1,31 @@
+a= 1
+print(a)
+b = "Puneeth"
+print(b )
+a1 = 9
+# print(a+b) -> Error can use "+" only btw same data type
+print(a+a1)
+print(type(a))
+
+'''
+    types of data type:
+    1.int
+    2.float
+    3.complex
+    4.Boolean
+    5.String
+    6.List
+    7.tuple
+    8.dictionary
+    9.set
+'''
+
+
+
+name = "puneeth" #string
+mark = [1,2,3] #list
+tup = (1,2,3)#tuple
+dictionary1 = {
+            "Puneeth":1,
+            "preetham":2,
+}#-> dictionary
