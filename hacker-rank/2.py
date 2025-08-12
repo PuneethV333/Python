764e96a0b6453d9e804ab6db6d98bd609f132fb3
PuneethV333 <puneethvijayakumar333@gmail.com>
2025-08-12 21:21:27 +0530

    Hacker-rank-day-1

---
+++ hacker-rank/2.py
@@ -0,0 +1,14 @@
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+n = int(input("Enter the number : "))
+if n%2 != 0:
+    print("Weird")
+elif (n>1 and n < 6):
+    print("Not Weird")
+elif (n>5 and n < 21):
+    print("Weird")
+else :
+    print("Not Weird")
